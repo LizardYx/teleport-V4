d9e8ccefb6a2ca02c61cd99cd7ac7c92a039c8b3
J_yx <563011491@qq.com>
2019-04-11 22:48:22 +0800

    - Init teleport V4 font end project

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      this is login page
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      input: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
