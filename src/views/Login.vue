bf746fc894cd801f9beb14cb68ac51937ce3cd53
J_yx <563011491@qq.com>
2019-04-23 00:07:39 +0800

    - add login page bg canvas

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,60 +1,57 @@
 <template>
     <div id="login">
+        <div id="bgCanvas"></div>
         <common-header></common-header>
         <div class="login-block">
-            <el-row>
-                <el-col :offset="16" :span="5">
-                    <div class="login-content">
-                        <div class="login-title">登陆</div>
-                        <el-tabs v-model="loginWay" class="login-detail" @tab-click="updateLoginWay" stretch>
-                            <el-tab-pane label="密码登陆" name="password">
-                                <el-form :model="loginInfo" status-icon ref="loginInfo">
-                                    <el-form-item prop="userName">
-                                        <el-input type="text" v-model="loginInfo.userName" placeholder="请输入用户名">
-                                        </el-input>
-                                    </el-form-item>
-                                    <el-form-item prop="password">
-                                        <el-input type="password" v-model="loginInfo.password" show-password placeholder="请输入密码">
-                                        </el-input>
-                                    </el-form-item>
-                                    <el-form-item prop="verificationCode">
-                                        <el-input type="text" v-model="loginInfo.verificationCode" placeholder="请输入验证码">
-                                            <template slot="append">.com</template>
-                                        </el-input>
-                                    </el-form-item>
-                                    <el-checkbox class="remember-me" v-model="loginInfo.rememberMe">记住我，12小时内免登陆</el-checkbox>
-                                    <el-form-item>
-                                        <el-button type="primary" class="login-btn">登陆</el-button>
-                                    </el-form-item>
-                                </el-form>
-                            </el-tab-pane>
-                            <el-tab-pane label="身份验证器登陆" name="authentication">
-                                <el-form :model="loginInfo" status-icon ref="loginInfo">
-                                    <el-form-item prop="userName">
-                                        <el-input type="text" v-model="loginInfo.userName" placeholder="请输入用户名">
-                                        </el-input>
-                                    </el-form-item>
-                                    <el-form-item prop="password">
-                                        <el-input type="password" v-model="loginInfo.password" show-password placeholder="请输入密码">
-                                        </el-input>
-                                    </el-form-item>
-                                    <el-form-item prop="verificationCode">
-                                        <el-input type="text" v-model="loginInfo.verificationCode" maxlength="6"
-                                                  placeholder="请输入六位数字身份器动态验证码">
-                                        </el-input>
-                                    </el-form-item>
-                                    <el-checkbox class="remember-me" v-model="loginInfo.rememberMe">记住我，12小时内免登陆</el-checkbox>
-                                    <el-form-item>
-                                        <el-button type="primary" class="login-btn">登陆</el-button>
-                                    </el-form-item>
-                                </el-form>
-                            </el-tab-pane>
-                            <router-link class="forgot-password" :to="{name: 'Login'}">忘记密码</router-link>
-                            <router-link class="authenticator" :to="{name: 'Login'}">绑定身份验证器</router-link>
-                        </el-tabs>
-                    </div>
-                </el-col>
-            </el-row>
+            <div class="login-content">
+                <div class="login-title">登陆</div>
+                <el-tabs v-model="loginWay" class="login-detail" @tab-click="updateLoginWay" stretch>
+                    <el-tab-pane label="密码登陆" name="password">
+                        <el-form :model="loginInfo" status-icon ref="loginInfo">
+                            <el-form-item prop="userName">
+                                <el-input type="text" v-model="loginInfo.userName" placeholder="请输入用户名">
+                                </el-input>
+                            </el-form-item>
+                            <el-form-item prop="password">
+                                <el-input type="password" v-model="loginInfo.password" show-password placeholder="请输入密码">
+                                </el-input>
+                            </el-form-item>
+                            <el-form-item prop="verificationCode">
+                                <el-input type="text" v-model="loginInfo.verificationCode" placeholder="请输入验证码">
+                                    <template slot="append">.com</template>
+                                </el-input>
+                            </el-form-item>
+                            <el-checkbox class="remember-me" v-model="loginInfo.rememberMe">记住我，12小时内免登陆</el-checkbox>
+                            <el-form-item>
+                                <el-button type="primary" class="login-btn">登陆</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </el-tab-pane>
+                    <el-tab-pane label="身份验证器登陆" name="authentication">
+                        <el-form :model="loginInfo" status-icon ref="loginInfo">
+                            <el-form-item prop="userName">
+                                <el-input type="text" v-model="loginInfo.userName" placeholder="请输入用户名">
+                                </el-input>
+                            </el-form-item>
+                            <el-form-item prop="password">
+                                <el-input type="password" v-model="loginInfo.password" show-password placeholder="请输入密码">
+                                </el-input>
+                            </el-form-item>
+                            <el-form-item prop="verificationCode">
+                                <el-input type="text" v-model="loginInfo.verificationCode" maxlength="6"
+                                          placeholder="请输入六位数字身份器动态验证码">
+                                </el-input>
+                            </el-form-item>
+                            <el-checkbox class="remember-me" v-model="loginInfo.rememberMe">记住我，12小时内免登陆</el-checkbox>
+                            <el-form-item>
+                                <el-button type="primary" class="login-btn">登陆</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </el-tab-pane>
+                    <router-link class="forgot-password" :to="{name: 'Login'}">忘记密码</router-link>
+                    <router-link class="authenticator" :to="{name: 'Login'}">绑定身份验证器</router-link>
+                </el-tabs>
+            </div>
         </div>
     </div>
 </template>
@@ -92,12 +89,15 @@
     #login{
         position: relative;
         height: 100vh;
-        background: #020815 url("../assets/login-bg.jpg");
+    }
+
+    #login #header{
+        position: fixed;
     }
 
     #login .login-block{
         position: absolute;
-        width: 100%;
+        width: 370px;
         height: 400px;
         margin: auto;
         top: 0;
@@ -119,6 +119,7 @@
         padding: 15px;
         background: #fff;
     }
+
     #login .login-detail{
         padding: 0 20px;
     }
