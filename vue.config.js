7d3521421846104d7640f9b1e32fa2211083c0a9
LizardYx <563011491@qq.com>
2019-06-11 18:58:08 +0800

    - update vue.config.js file

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
 const path = require("path");
 module.exports = {
-    // 生产环境是否生成 sourceMap 文件
-    productionSourceMap: false,
+    // 部署应用包时的基本 URL
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+    // 当运行 vue-cli-service build 时构建文件的目录
+    outputDir: 'dist',
+    // 生成的静态资源文件名中是否包含 hash
+    filenameHashing: true,
+    // 生产环境的 source map
+    productionSourceMap: true,
+    // 如果你构建后的文件是部署在 CDN 上的，启用该选项可以提供额外的安全性
+    integrity: false,
     // css相关配置
     css: {
         loaderOptions: {
